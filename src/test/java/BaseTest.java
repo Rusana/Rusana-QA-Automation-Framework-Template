42606eb87fefea73322f3c21e37dda817028b861
Rusana <shikhabidovarusana@gmail.com>
2023-10-18 11:11:57 +1100

    HomeWork_17
    Can't find  locater song. Thank you.

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,14 +1,41 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
 public class BaseTest {
-//    WebDriver wait;
+    public static WebDriver driver = null;
+//    public static String url = "https://qa.koel.app/"; //added
 
     @BeforeSuite
-    static void setupClass() {
+    public static void setupClass() {
         WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeMethod
+    public static void setUpBrowser() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://qa.koel.app/"; //added
+        driver.get(url);
+    }
+@AfterMethod
+public static void  tearDown () {
+
+    driver.quit();
+
+}
 
     }
-}
+
+
+
--- src/test/java/HomeWork17.java
+++ src/test/java/HomeWork17.java
@@ -5,32 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import  java.time.Duration ;
-
-
-
+import  java.time.Duration ; //added
+import java.util.UUID; //added
 
 public class HomeWork17 extends BaseTest{
     @Test
-
-
-    public void addSongToPlaylist (){
-//        setupEmail("shikhabidovarusana@gmail.com");
-
-        ChromeOptions options=new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url= "https://qa.koel.app/";
-        driver.get(url);
-
+    public void addSongToPlaylist () throws InterruptedException {
         WebElement logEmail= driver.findElement(By.xpath("//input[@type='email']"));
         logEmail.click();
         logEmail.clear();
         logEmail.sendKeys("shikhabidovarusana@gmail.com");
-//?        logEmail.sendKeys("email");
         WebElement logPassword= driver.findElement(By.cssSelector("[type='password']"));
         logPassword.click();
         logPassword.clear();
@@ -39,27 +23,21 @@ public class HomeWork17 extends BaseTest{
         WebElement logIn=driver.findElement(By.cssSelector("[type='submit']"));
         logIn.click();
 
-        WebElement playListLink =driver.findElement(By.cssSelector("[class='fa fa-plus-circle create']"));
-        playListLink.click();
+        WebElement allSong =driver.findElement(By.cssSelector("[class='songs']"));
+        allSong.click();
 
-        WebElement newPlaylist = driver.findElement(By.xpath("//li[.='New Playlist']"));
-        newPlaylist.click();
+        WebElement songAdd = driver.findElement(By.cssSelector("section#songResultsWrapper tr.song.item td.title"));
+        songAdd.click();
 
         WebElement name= driver.findElement(By.cssSelector("[name='name']"));
         name.click();
         name.clear();
-        name.sendKeys("Algorithms");
-
+        name.sendKeys("Hi");
+Thread.sleep(5000); //slow go for good to see your work in IJ
         String playListUrl="https://qa.koel.app/#!/home";
 
         Assert.assertEquals(driver.getCurrentUrl(),playListUrl);
-        driver.quit();
-
-//
-//    }
-//
-//    private void setupEmail() {
-
+//        driver.quit();
 
     }
 }
