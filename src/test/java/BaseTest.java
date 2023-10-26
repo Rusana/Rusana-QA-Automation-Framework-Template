734f90ff5f7e1b1faafcfe6bb562382ce266359b
Rusana <shikhabidovarusana@gmail.com>
2023-10-27 10:55:57 +1100

    HomeWork_17
    Good Job!

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,39 +1,37 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-
-import java.time.Duration;
+import io.github.bonigarcia.wdm.WebDriverManager; // Импорт библиотеки для управления веб-драйвером
+import org.openqa.selenium.WebDriver; // Импорт интерфейса WebDriver для взаимодействия с браузером
+import org.openqa.selenium.chrome.ChromeDriver;  // Импорт класса ChromeDriver для управления браузером Google Chrome
+import org.openqa.selenium.chrome.ChromeOptions; // Импорт класса ChromeOptions для настройки параметров Chrome
+import org.testng.annotations.AfterMethod; // Импорт аннотации для завершения метода после каждого тестового метода
+import org.testng.annotations.BeforeMethod;  // Импорт аннотации для выполнения метода перед каждым тестовым методом
+import org.testng.annotations.BeforeSuite; // Импорт аннотации для выполнения метода перед всеми тестами в наборе тестов
+import java.time.Duration; // Импорт класса Duration для работы с временем
 
 public class BaseTest {
-    public static WebDriver driver = null;
+    public static WebDriver driver = null; // Объявление статической переменной driver, представляющей веб-драйвер
+
 //    public static String url = "https://qa.koel.app/"; //added
 
-    @BeforeSuite
+    @BeforeSuite // Метод, выполняющийся перед всеми тестами в наборе тестов
     public static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+        WebDriverManager.chromedriver().setup();  // Настройка ChromeDriver с использованием WebDriverManager
     }
-
-    @BeforeMethod
+    @BeforeMethod  // Метод, выполняющийся перед каждым тестовым методом
     public static void setUpBrowser() {
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        String url = "https://qa.koel.app/"; //added
-        driver.get(url);
+        options.addArguments("--disable-notifications","--remote-allow-origins=*", "--incognito","--start-maximized");
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+// Установка опции исключения для обхода автоматизации
+        driver = new ChromeDriver(options); // Инициализация веб-драйвера Chrome с заданными опциями
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Установка неявного ожидания (10 секунд)
+        String url = "https://qa.koel.app/"; // Задание URL-адреса, который будет открыт в браузере
+        driver.get(url); // Открытие указанной URL-страницы
     }
-@AfterMethod
+    @AfterMethod // Метод, выполняющийся после каждого тестового метода
 public static void  tearDown () {
-
-    driver.quit();
-
-}
+        driver.quit(); // Завершение сеанса браузера и освобождение ресурсов
+    }
 
     }
 
--- src/test/java/HomeWork17.java
+++ src/test/java/HomeWork17.java
@@ -1,49 +1,69 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.Assert;
-import org.testng.annotations.Test;
-import  java.time.Duration ; //added
-import java.util.UUID; //added
+import org.openqa.selenium.By; // Импорт класса By для поиска элементов на веб-странице
+import org.openqa.selenium.WebElement; // Импорт класса WebElement для представления элементов на веб-странице
+import org.testng.Assert; // Импорт класса Assert для проверки утверждений в тестах
+import org.testng.annotations.Test; // Импорт аннотации для обозначения тестового метода
 
-public class HomeWork17 extends BaseTest{
-    @Test
-    public void addSongToPlaylist () throws InterruptedException {
-        WebElement logEmail= driver.findElement(By.xpath("//input[@type='email']"));
+public class HomeWork17 extends BaseTest { // Объявление класса HomeWork17, который наследует функциональность класса BaseTest
+    String expectedSongAddedMessage = "Added 1 song into";
+
+    @Test // Обозначение метода как тестового метода с использованием аннотации @Test
+    public void addSongToPlaylist() throws InterruptedException {
+        // Поиск и взаимодействие с элементами веб-страницы
+
+        // Поиск элемента для ввода email
+        WebElement logEmail = driver.findElement(By.xpath("//input[@type='email']"));
         logEmail.click();
         logEmail.clear();
         logEmail.sendKeys("shikhabidovarusana@gmail.com");
-        WebElement logPassword= driver.findElement(By.cssSelector("[type='password']"));
+
+        // Поиск элемента для ввода пароля
+        WebElement logPassword = driver.findElement(By.cssSelector("[type='password']"));
         logPassword.click();
         logPassword.clear();
         logPassword.sendKeys("te$t$tudent");
 
-        WebElement buttonLogIn=driver.findElement(By.cssSelector("[type='submit']"));
+        // Поиск и клик по кнопке "Войти"
+        WebElement buttonLogIn = driver.findElement(By.cssSelector("[type='submit']"));
         buttonLogIn.click();
+        Thread.sleep(2000);
 
-        WebElement buttonAllSong =driver.findElement(By.cssSelector("[class='songs']"));
+        // Переход к разделу "Все песни"
+        WebElement buttonAllSong = driver.findElement(By.cssSelector("[class='songs']"));
         buttonAllSong.click();
         Thread.sleep(2000);
 
-        WebElement nameSong= driver.findElement(By.xpath("//tr[@class='song-item playing selected']//td[@class='title']"));
+        // Поиск и клик по имени песни (например, "Pluto")
+        WebElement nameSong = driver.findElement(By.xpath("//section[@id='songsWrapper']//td[contains(text(),'Pluto')]"));
         nameSong.click();
+        Thread.sleep(500);
 
-        WebElement choseSong = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//tr[@class='song-item']"));
-        choseSong.click();
-
-        WebElement buttonAddSong= driver.findElement(By.xpath("//button[@class='btn-add-to']"));
+        // Поиск и клик по кнопке "Добавить песню в плейлист"
+        WebElement buttonAddSong = driver.findElement(By.xpath("//button[@class='btn-add-to']"));
         buttonAddSong.click();
-//
-        WebElement newPlayList= driver.findElement(By.cssSelector(""));
-        newPlayList.clear();
-        newPlayList.sendKeys("Hi");
-Thread.sleep(5000); //slow go for good to see your work in IJ
-        String playListUrl="https://qa.koel.app/#!/home";
+        Thread.sleep(500);
 
-        Assert.assertEquals(driver.getCurrentUrl(),playListUrl);
-//        driver.quit();
+        // Поиск и клик по элементу "Плейлист"
+        WebElement playlist = driver.findElement(By.cssSelector("#songsWrapper .playlist"));
+        playlist.click();
+        Thread.sleep(500);
 
+
+//        String expectedSongAddedMessage = "Added 1 song into \"Test Pro PlayList.\""; // Ожидаемое сообщение об успешном добавлении песни в плейлист
+//        Assert.assertTrue(getNotificationText().contains(expectedSongAddedMessage));
+//        Assert.assertTrue(getNotificationText().contains(expectedSongAddedMessage));
+
+
+        // Проверка фактичес (). contains(expectedSongAddedMessage));
     }
+
+    // Метод для получения текстового сообщения об успешном добавлении песни в плейлист
+    public String getNotificationText() {
+
+        WebElement notificationElement = driver.findElement(By.cssSelector("div.success.show"));
+
+        Assert.assertTrue(getNotificationText().contains(expectedSongAddedMessage));
+
+        return notificationElement.getText();
+    }
+
 }
