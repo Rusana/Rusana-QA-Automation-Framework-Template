d959a1d0f4b53e783f25e1b85dae94b1967e9d65
Rusana <shikhabidovarusana@gmail.com>
2023-11-28 03:01:27 +1100

    Homework20
    HI!

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -15,12 +16,13 @@ import static org.testng.Assert.assertTrue;
 
 public class BaseTest {
     public static WebDriver driver = null; // Объявление статической переменной driver, представляющей веб-драйвер
-
+public static WebDriverWait wait;
 //    public static String url = "https://qa.koel.app/"; //added
 
     @BeforeSuite // Метод, выполняющийся перед всеми тестами в наборе тестов
     public static void setupClass() {
         WebDriverManager.chromedriver().setup();  // Настройка ChromeDriver с использованием WebDriverManager
+
     }
 
     @BeforeMethod  // Метод, выполняющийся перед каждым тестовым методом
@@ -34,7 +36,10 @@ public class BaseTest {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10)); // Установка неявного ожидания (10 секунд)
         String url = "https://qa.koel.app/"; // Задание URL-адреса, который будет открыт в браузере
         driver.get(url); // Открытие указанной URL-страницы
+        wait = new WebDriverWait(driver, Duration.ofSeconds(4)); // Method explicit wait
+
     }
+
 
     @AfterMethod // Метод, выполняющийся после каждого тестового метода
     public static void tearDown() {
