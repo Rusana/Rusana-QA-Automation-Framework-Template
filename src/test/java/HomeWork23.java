ac1b0e28bb8e1304181ca6cacb32b5511d37db5a
Rusana <shikhabidovarusana@gmail.com>
2023-12-21 12:03:34 +1100

    HomeWork23
    Hello! I'm done!)

---
+++ src/test/java/HomeWork23.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.ArtistsPage;
+import pageObjects.LoginPage;
+
+public class HomeWork23 extends BaseTest {
+    @Test
+
+    public void PageFactory() throws InterruptedException {
+
+        LoginPage loginPage = new LoginPage(driver);
+        ArtistsPage artistsPage = new ArtistsPage(driver);
+        PageFactory.initElements(driver, this);
+
+        loginPage.enterEmail("shikhabidovarusana@gmail.com");
+        loginPage.enterPassword("te$t$tudent");
+        loginPage.clickSubmit();
+        Thread.sleep(500);
+        Assert.assertTrue(artistsPage.artists());
+
+
+    }
+}
--- src/test/java/pageObjects/BasePage.java
+++ src/test/java/pageObjects/BasePage.java
@@ -2,6 +2,7 @@ package pageObjects;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -16,7 +17,7 @@ public class BasePage {
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         actions = new Actions(driver);
-
+        PageFactory.initElements(driver, this);
 
     }
 
